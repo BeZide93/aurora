4a3fa010ab1228f73f6ff5b872933f63f94fb8d4
BeZide93 <r.wolter93@gmx.de>
2026-05-13 20:08:49 +0200

    Normalize Dusk shoulder mappings

--- lib/dolphin/pad/pad.cpp
+++ lib/dolphin/pad/pad.cpp
@@ -467,6 +467,33 @@ void __PADSetDefaultMapping(aurora::input::GameController* controller) /*  NOLIN
   }
 }
 
+static PADButtonMapping* find_button_mapping(aurora::input::GameController* controller, PADButton button) {
+  auto iter = std::find_if(controller->m_buttonMapping.begin(), controller->m_buttonMapping.end(),
+                           [button](const auto& mapping) { return mapping.padButton == button; });
+  return iter == controller->m_buttonMapping.end() ? nullptr : &*iter;
+}
+
+static void __PADApplyDuskButtonMappingDefaults(aurora::input::GameController* controller) {
+  PADButtonMapping* zl = find_button_mapping(controller, PAD_TRIGGER_ZL);
+  if (zl == nullptr) {
+    return;
+  }
+
+  PADButtonMapping* a = find_button_mapping(controller, PAD_BUTTON_A);
+  const bool zlOnA = a != nullptr && a->nativeButton != PAD_NATIVE_BUTTON_INVALID && zl->nativeButton == a->nativeButton;
+  PADButtonMapping* l = find_button_mapping(controller, PAD_TRIGGER_L);
+  if (zlOnA) {
+    zl->nativeButton = PAD_NATIVE_BUTTON_INVALID;
+  }
+
+  if (zl->nativeButton == SDL_GAMEPAD_BUTTON_LEFT_SHOULDER) {
+    if (l != nullptr) {
+      l->nativeButton = SDL_GAMEPAD_BUTTON_LEFT_SHOULDER;
+    }
+    zl->nativeButton = PAD_NATIVE_BUTTON_INVALID;
+  }
+}
+
 void __PADLoadMapping(aurora::input::GameController* controller) /*  NOLINT(*-reserved-identifier) */ {
   int32_t playerIndex = SDL_GetGamepadPlayerIndex(controller->m_controller);
   if (playerIndex == -1) {
@@ -485,6 +512,7 @@ void __PADLoadMapping(aurora::input::GameController* controller) /*  NOLINT(*-re
                                 controller->m_pid);
   SDL_IOStream* file = SDL_IOFromFile(path.c_str(), "rb");
   if (file == nullptr) {
+    __PADApplyDuskButtonMappingDefaults(controller);
     return;
   }
 
@@ -492,6 +520,7 @@ void __PADLoadMapping(aurora::input::GameController* controller) /*  NOLINT(*-re
   SDL_ReadU32LE(file, &magic);
   if (magic != SBIG('CTRL')) {
     aurora::input::Log.warn("Invalid controller mapping magic!");
+    __PADApplyDuskButtonMappingDefaults(controller);
     return;
   }
 
@@ -500,6 +529,7 @@ void __PADLoadMapping(aurora::input::GameController* controller) /*  NOLINT(*-re
   if (version != k_mappingsFileVersion) {
     aurora::input::Log.warn("Invalid controller mapping version! (Expected {0}, found {1})", k_mappingsFileVersion,
                             version);
+    __PADApplyDuskButtonMappingDefaults(controller);
     return;
   }
 
@@ -552,6 +582,7 @@ void __PADLoadMapping(aurora::input::GameController* controller) /*  NOLINT(*-re
                             playerIndex);
     __PADSetDefaultMapping(controller);
   }
+  __PADApplyDuskButtonMappingDefaults(controller);
 }
 
 static void EnsureMappingLoaded(aurora::input::GameController* controller) {
@@ -1465,6 +1496,7 @@ void PADRestoreDefaultMapping(const u32 port) {
     return;
   }
   __PADSetDefaultMapping(controller);
+  __PADApplyDuskButtonMappingDefaults(controller);
   controller->m_axisMapping = g_defaultAxes;
 }
 
